35c1621e2d7830085bd55fac883780dd64db4a90
ayussh-2 <ayushatschool5@gmail.com>
2024-05-25 21:10:31 +0530

    chore: Update prices page

--- src/app-price.jsx
+++ src/app-price.jsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { auth } from "./firebase";
-import { Api } from "@mui/icons-material";
+import { motion } from "framer-motion";
+
 function JsonDataDisplay() {
     const [username, setUser] = useState("");
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
             if (user) {
@@ -27,14 +29,17 @@ function JsonDataDisplay() {
 
     const API = `https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd0000019220782169bf4ab67fc27b9390fd8a8a&format=json&offset=${offset}`;
 
-    const fetchArticles = async (url) => {
+    const fetchArticles = async () => {
         try {
-            console.log(API);
+            // console.log(API);
+            setLoading(true);
             const res = await fetch(API);
             const data = await res.json();
             setPrice(data.records);
         } catch (e) {
             console.error(e);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -57,60 +62,103 @@ function JsonDataDisplay() {
     };
 
     return (
-        <div className=" bg-[#dde7c7]">
+        <motion.div
+            animate={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: 500 }}
+            exit={{ opacity: 0, y: 500 }}
+            transition={{
+                duration: 0.5,
+                ease: [0.2, 1, 0.2, 1],
+            }}
+            className=" bg-[#dde7c7] font-poppins"
+        >
             <Header date={date} name={username} />
 
-            <div className="h-screen mt-5">
-                <p className="flex justify-center font-medium text-2xl p-4">
-                    Prices of Daily Mandi Sale Prices - {date}
-                </p>
+            {loading ? (
+                <div className="flex items-center justify-center h-screen">
+                    <span className="loading loading-infinity loading-lg scale-125"></span>
+                </div>
+            ) : (
+                <div className="h-screen mt-5">
+                    <p className="flex justify-center font-medium text-2xl p-4">
+                        Prices of Daily Mandi Sale Prices - {date}
+                    </p>
 
-                <div className="flex gap-3 justify-center p-2">
-                    <button
-                        className="btn text-black"
-                        onClick={() => {
-                            Tog2();
-                        }}
-                    >
-                        Previous Page
-                    </button>
-                    <button
-                        className="btn text-black"
-                        onClick={() => {
-                            Tog1();
-                        }}
-                    >
-                        Next Page
-                    </button>
-                </div>
-                <div className="px-10 mt-5">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>State</th>
-                                <th>Commodity</th>
-                                <th>Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {console.log(price)}
-                            {price.map((props) => {
-                                return (
+                    <div className="flex gap-3 justify-center p-2">
+                        <button
+                            className="btn text-black"
+                            onClick={() => {
+                                Tog2();
+                            }}
+                        >
+                            Previous Page
+                        </button>
+                        <button
+                            className="btn text-black"
+                            onClick={() => {
+                                Tog1();
+                            }}
+                        >
+                            Next Page
+                        </button>
+                    </div>
+                    <div className="px-10 mt-5">
+                        <div className="overflow-x-auto p-5">
+                            <table className="min-w-full divide-y divide-black rounded-xl outline outline-[1px] outline-black">
+                                <thead className="bg-[#dde7c7]">
                                     <tr>
-                                        <td>{props.state}</td>
-                                        <td>{props.commodity}</td>
-                                        <td>
-                                            ₹⟩{props.modal_price / 50} / KG{" "}
-                                        </td>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-base font-medium text-black uppercase tracking-wider"
+                                        >
+                                            State
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-base font-medium text-black uppercase tracking-wider"
+                                        >
+                                            Commodity
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-base font-medium text-black uppercase tracking-wider"
+                                        >
+                                            Price
+                                        </th>
                                     </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
+                                </thead>
+                                <tbody className="">
+                                    {price.map((props, index) => (
+                                        <tr key={props.state + props.commodity}>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="text-sm text-gray-900">
+                                                    {props.state}
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="text-sm text-gray-900">
+                                                    {props.commodity}
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="text-sm text-gray-900">
+                                                    ₹&nbsp;
+                                                    {(
+                                                        props.modal_price / 50
+                                                    ).toFixed(2)}{" "}
+                                                    / KG
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            )}
             <Footer />
-        </div>
+        </motion.div>
     );
 }
 
